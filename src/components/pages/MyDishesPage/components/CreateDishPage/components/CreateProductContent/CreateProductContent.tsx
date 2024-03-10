0c6ebad56ec4ff66a76c559d6477ece5b6dacae8
Julia <yuliabobrovich@yandex.ru>
2024-03-10 16:57:19 +0300

    DIPL-21 Almost finished create- dish/product page

--- src/components/pages/MyDishesPage/components/CreateDishPage/components/CreateProductContent/CreateProductContent.tsx
+++ src/components/pages/MyDishesPage/components/CreateDishPage/components/CreateProductContent/CreateProductContent.tsx
@@ -1,9 +1,220 @@
 import React from "react";
 
+import CloseIcon from "@mui/icons-material/Close";
+import cn from "classnames";
+import { observer } from "mobx-react-lite";
+
 import styles from "./styles.module.scss";
+import { Button } from "@/components/Button";
+import { CommonAccordion } from "@/components/CommonAccordion";
+import { Counter } from "@/components/Counter";
+import { Input } from "@/components/Input";
+import CreateProductContentStore from "@/store/CreateProductContentStore";
+import { useLocalStore } from "@/utils/useLocalStore";
 
 const CreateProductContent: React.FC = () => {
-  return <div className={styles.createProductContent}></div>;
+  const createProductContentStore = useLocalStore(
+    () => new CreateProductContentStore(),
+  );
+
+  React.useEffect(() => {
+    createProductContentStore.requestServingSizes();
+  }, []);
+
+  return (
+    <div className={styles.createProductContent}>
+      <Input
+        onChange={(value: string) => {
+          createProductContentStore.setName(value);
+        }}
+        placeholder="Название"
+        className={styles.createProductContent_input}
+        containerClassName={styles.createProductContent_input_container}
+      />
+      <textarea
+        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+          createProductContentStore.setDescription(event.target.value);
+        }}
+        className={styles.createProductContent_description}
+        placeholder="Описание"
+      />
+      <Input
+        onChange={(value: string) => {
+          createProductContentStore.setImage(value);
+        }}
+        placeholder="Изображение"
+        className={styles.createProductContent_input}
+        containerClassName={styles.createProductContent_input_container}
+      />
+      <div className={styles.createProductContent_block}>
+        <span className={styles.createProductContent_text}>
+          Энергия (Ккал):
+        </span>
+        <Counter
+          onChange={(value: number) => {
+            createProductContentStore.setEnergy(value);
+          }}
+          input={true}
+          className={styles.createProductContent_counter}
+        />
+      </div>
+      <div className={styles.createProductContent_block}>
+        <span className={styles.createProductContent_text}>
+          Белки (мг/100 г.):
+        </span>
+        <Counter
+          onChange={(value: number) => {
+            createProductContentStore.setProtein(value);
+          }}
+          input={true}
+          className={styles.createProductContent_counter}
+        />
+      </div>
+      <div className={styles.createProductContent_block}>
+        <span className={styles.createProductContent_text}>
+          Жиры (мг/100 г.):
+        </span>
+        <Counter
+          onChange={(value: number) => {
+            createProductContentStore.setFat(value);
+          }}
+          input={true}
+          className={styles.createProductContent_counter}
+        />
+      </div>
+      <div className={styles.createProductContent_block}>
+        <span className={styles.createProductContent_text}>
+          Углеводы (мг/100 г.):
+        </span>
+        <Counter
+          onChange={(value: number) => {
+            createProductContentStore.setCarbs(value);
+          }}
+          input={true}
+          className={styles.createProductContent_counter}
+        />
+      </div>
+      <CommonAccordion
+        title="Порции"
+        className={styles.createProductContent_accordion}
+      >
+        <div className={styles.createProductContent_servingSizes}>
+          <div className={styles.createProductContent_servingSizes_current}>
+            <h2 className={styles.createProductContent_servingSizes_h}>
+              Добавить
+            </h2>
+            {createProductContentStore.servingSizes.map((size) => {
+              let grams: number = 0;
+
+              return (
+                <div
+                  key={size.id}
+                  className={
+                    styles.createProductContent_servingSizes_current_size
+                  }
+                >
+                  <span
+                    className={
+                      styles.createProductContent_servingSizes_current_size_text
+                    }
+                  >
+                    {size.name}
+                  </span>
+                  <Counter
+                    defaultNumber={grams}
+                    onChange={(value: number) => {
+                      grams = value;
+                    }}
+                    input={true}
+                    className={cn(
+                      styles.createProductContent_counter,
+                      styles.createProductContent_servingSizes_current_size_counter,
+                    )}
+                  />
+                  <span
+                    className={
+                      styles.createProductContent_servingSizes_current_size_creation
+                    }
+                  >
+                    Грамм
+                  </span>
+                  <Button
+                    onClick={() => {
+                      createProductContentStore.addSelectedServingSize({
+                        ...size,
+                        grams,
+                      });
+                      grams = 0;
+                    }}
+                    className={
+                      styles.createProductContent_servingSizes_current_size_creation
+                    }
+                  >
+                    +
+                  </Button>
+                </div>
+              );
+            })}
+          </div>
+          <div className={styles.createProductContent_servingSizes_selected}>
+            <h2 className={styles.createProductContent_servingSizes_h}>
+              Добавлено
+            </h2>
+            {createProductContentStore.selectedServingSizes.map((size) => {
+              return (
+                <div
+                  key={size.id}
+                  className={
+                    styles.createProductContent_servingSizes_selected_size
+                  }
+                >
+                  <span
+                    className={
+                      styles.createProductContent_servingSizes_selected_size_creation
+                    }
+                  >
+                    {size.name}
+                  </span>
+                  <span
+                    className={
+                      styles.createProductContent_servingSizes_selected_size_creation
+                    }
+                  >
+                    {size.grams}
+                  </span>
+                  <span
+                    className={
+                      styles.createProductContent_servingSizes_selected_size_creation
+                    }
+                  >
+                    г.
+                  </span>
+                  <CloseIcon
+                    onClick={() => {
+                      createProductContentStore.removeSelectedServingSize(
+                        size.id,
+                      );
+                    }}
+                    className={
+                      styles.createProductContent_servingSizes_selected_size_close
+                    }
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </CommonAccordion>
+      <Button
+        onClick={() => {
+          createProductContentStore.sendProduct();
+        }}
+        className={styles.createProductContent_btn}
+      >
+        Создать
+      </Button>
+    </div>
+  );
 };
 
-export default CreateProductContent;
+export default observer(CreateProductContent);
--- src/store/CreateProductContentStore.ts
+++ src/store/CreateProductContentStore.ts
@@ -1,15 +1,106 @@
-import { makeObservable, observable } from "mobx";
+import axios from "axios";
+import {
+  action,
+  computed,
+  makeObservable,
+  observable,
+  runInAction,
+} from "mobx";
 
+import { NutrientsType, ServingSizeType } from "./CreateDishContentStore";
+import { HOST } from "@/config/host";
 import { ILocalStore } from "@/utils/useLocalStore";
 
-type PrivateFields = "_name";
+export type ServingSizeWithGramsType = ServingSizeType & {
+  grams: number;
+};
+
+type PrivateFields =
+  | "_name"
+  | "_description"
+  | "_image"
+  | "_energy"
+  | "_protein"
+  | "_fat"
+  | "_carbs"
+  | "_nutrients"
+  | "_servingSizes"
+  | "_selectedServingSizes";
 
 class CreateProductContentStore implements ILocalStore {
   private _name: string = "";
+  private _description: string = "";
+  private _image: string | ImageData = "";
+  private _energy: number | null = null;
+  private _protein: number | null = null;
+  private _fat: number | null = null;
+  private _carbs: number | null = null;
+  private _nutrients: NutrientsType = {
+    energy: 0,
+    calories: 0,
+    fats: {
+      total: 0,
+      saturated: 0,
+      polyunsaturated: 0,
+      monounsaturated: 0,
+      "trans-fats": 0,
+    },
+    carbohydrates: {
+      total: 0,
+      fiber: 0,
+      sugar: 0,
+    },
+    protein: 0,
+    cholesterol: 0,
+    iron: 0,
+    sodium: 0,
+    potassium: 0,
+    calcium: 0,
+    vitamins: {
+      vitaminA: 0,
+      vitaminB: 0,
+      vitaminC: 0,
+      vitaminD: 0,
+      vitaminE: 0,
+      vitaminK: 0,
+    },
+  };
+  private _servingSizes: ServingSizeType[] = [];
+  private _selectedServingSizes: ServingSizeWithGramsType[] = [];
 
   constructor() {
     makeObservable<CreateProductContentStore, PrivateFields>(this, {
       _name: observable,
+      setName: action,
+      name: computed,
+      _description: observable,
+      setDescription: action,
+      description: computed,
+      _image: observable,
+      setImage: action,
+      image: computed,
+      _energy: observable,
+      setEnergy: action,
+      energy: computed,
+      _protein: observable,
+      setProtein: action,
+      protein: computed,
+      _fat: observable,
+      setFat: action,
+      fat: computed,
+      _carbs: observable,
+      setCarbs: action,
+      carbs: computed,
+      _nutrients: observable,
+      setNutrients: action,
+      nutrients: computed,
+      _servingSizes: observable,
+      setServingSizes: action,
+      servingSizes: computed,
+      _selectedServingSizes: observable,
+      setSelectedServingSizes: action,
+      selectedServingSizes: computed,
+      addSelectedServingSize: action,
     });
   }
 
@@ -19,6 +110,142 @@ class CreateProductContentStore implements ILocalStore {
 
   get name() {
     return this._name;
+  }
+
+  setDescription(description: string) {
+    this._description = description;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  setImage(image: string | ImageData) {
+    this._image = image;
+  }
+
+  get image() {
+    return this._image;
+  }
+
+  setEnergy(energy: number) {
+    this._energy = energy;
+  }
+
+  get energy() {
+    return this._energy;
+  }
+
+  setProtein(protein: number | null) {
+    this._protein = protein;
+  }
+
+  get protein() {
+    return this._protein;
+  }
+
+  setFat(fat: number | null) {
+    this._fat = fat;
+  }
+
+  get fat() {
+    return this._fat;
+  }
+
+  setCarbs(carbs: number | null) {
+    this._carbs = carbs;
+  }
+
+  get carbs() {
+    return this._carbs;
+  }
+
+  setNutrients(nutrients: NutrientsType) {
+    this._nutrients = nutrients;
+  }
+
+  get nutrients() {
+    return this._nutrients;
+  }
+
+  setServingSizes(servingSizes: ServingSizeType[]) {
+    this._servingSizes = servingSizes;
+  }
+
+  get servingSizes() {
+    return this._servingSizes;
+  }
+
+  async requestServingSizes() {
+    try {
+      const result = await axios({
+        url: `${HOST}/serving_sizes`,
+        method: "GET",
+      });
+
+      runInAction(() => {
+        this.setServingSizes(result.data);
+      });
+    } catch (e) {
+      console.log("CreateProductContentStore: ", e);
+    }
+  }
+
+  setSelectedServingSizes(selectedServingSizes: ServingSizeWithGramsType[]) {
+    this._selectedServingSizes = selectedServingSizes;
+  }
+
+  get selectedServingSizes() {
+    return this._selectedServingSizes;
+  }
+
+  addSelectedServingSize(selectedServingSize: ServingSizeWithGramsType) {
+    const exists = this._selectedServingSizes.find(
+      (size) => size.id == selectedServingSize.id,
+    );
+    if (exists) {
+      this.removeSelectedServingSize(selectedServingSize.id);
+      this._selectedServingSizes.push(selectedServingSize);
+    } else {
+      this._selectedServingSizes.push(selectedServingSize);
+    }
+  }
+
+  removeSelectedServingSize(id: number | string) {
+    this.setSelectedServingSizes(
+      this._selectedServingSizes.filter((size) => size.id != id),
+    );
+  }
+
+  async sendProduct() {
+    try {
+      const product = {
+        name: this.name,
+        description: this.description,
+        image: this.image,
+        energy: this.energy,
+        protein: this.protein,
+        carbs: this.carbs,
+        fat: this.fat,
+        nutrients: this.nutrients,
+        serving_sizes: this.selectedServingSizes.map((size) => ({
+          id: size.id,
+          grams: size.grams,
+        })),
+      };
+
+      await axios({
+        url: `${HOST}/products`,
+        method: "POST",
+        data: product,
+      });
+
+      runInAction(() => {
+        alert(`Продукт ${this.name} успешно создан!`);
+      });
+    } catch (e) {
+      console.log("CreateProductContentStore: ", e);
+    }
   }
 
   destroy() {}
